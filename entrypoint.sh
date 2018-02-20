d67e1a7682d164a78c9fb7e06b214d94fed048ae
Sami Koskinen <sami@richie.fi>
2018-02-20 16:11:46 +0200

    $CACHE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-exec ./imageproxy -addr "0.0.0.0:$PORT" -urlPrefix "$URL_PREFIX" -baseURL "$BASE_URL" -forbidAbsoluteURLs "$FORBID_ABSOLUTE_URLS"
+exec ./imageproxy -addr "0.0.0.0:$PORT" -urlPrefix "$URL_PREFIX" -baseURL "$BASE_URL" -forbidAbsoluteURLs "$FORBID_ABSOLUTE_URLS" -cache "$CACHE"
