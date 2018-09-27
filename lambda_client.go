831c38c61f90e655ebff9cb82f87516abf0f610d
Sami Koskinen <sami@richie.fi>
2018-09-27 17:36:08 +0300

    Call Lambda synchronously instead of transforming in the server process

--- lambda_client.go
+++ lambda_client.go
@@ -1,7 +1,13 @@
 package imageproxy
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/url"
+
+	"github.com/aws/aws-sdk-go/aws"
+	awssession "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
 
 	"github.com/richiefi/imageproxy/options"
 )
@@ -16,7 +22,53 @@ type LambdaTransformResponse struct {
 	Image  []byte `json:"i"`
 }
 
-func TransformWithURL(u *url.URL) (int, []byte, error) {
+type LambdaClient interface {
+	TransformWithURL(*url.URL, options.Options) (int, []byte, error)
+}
+
+type lambdaClient struct {
+	c            *lambda.Lambda
+	functionName string
+}
+
+func NewLambdaClient(functionName string) (LambdaClient, error) {
+	session, err := awssession.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	client := lambdaClient{}
+	client.c = lambda.New(session)
+	client.functionName = functionName
+	return &client, nil
+}
+
+func (c *lambdaClient) TransformWithURL(u *url.URL, options options.Options) (int, []byte, error) {
 	// TransformWithURL will call Lambda synchronously and executes DoTransformWithURL there
-	return 0, nil, nil
+	if u == nil {
+		return 0, nil, fmt.Errorf("URL can't be nil")
+	}
+
+	reqPayload, err := json.Marshal(LambdaTransformRequest{
+		URLString: u.String(),
+		Options:   options,
+	})
+	if err != nil {
+		return 0, nil, err
+	}
+
+	invokeOutput, err := c.c.Invoke(&lambda.InvokeInput{
+		FunctionName:   aws.String(c.functionName),
+		InvocationType: aws.String("RequestResponse"), // this makes it synchronous
+		Payload:        reqPayload,
+	})
+	if err != nil {
+		return 0, nil, err
+	}
+
+	var resp LambdaTransformResponse
+	err = json.Unmarshal(invokeOutput.Payload, &resp)
+	if err != nil {
+		return 0, nil, err
+	}
+	return resp.Status, resp.Image, nil
 }
