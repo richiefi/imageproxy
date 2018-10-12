1c3bcebf8151acf4f2d11c4d544c0a3306c7b738
Sami Koskinen <sami@richie.fi>
2018-10-12 15:02:02 +0300

    LambdaTransformResponse JSON decoding/encoding with support for ascii85

--- lambda_client.go
+++ lambda_client.go
@@ -1,10 +1,15 @@
 package imageproxy
 
 import (
+	"bytes"
+	"encoding/ascii85"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	awssession "github.com/aws/aws-sdk-go/aws/session"
@@ -19,9 +24,69 @@ type LambdaTransformRequest struct {
 }
 
 type LambdaTransformResponse struct {
-	Status         int         `json:"st"`
-	UpstreamHeader http.Header `json:"uh"`
-	Image          []byte      `json:"im"`
+	Status         int
+	UpstreamHeader http.Header
+	Image          []byte
+}
+
+func (r LambdaTransformResponse) MarshalJSON() ([]byte, error) {
+	var withString struct {
+		Status         int         `json:"st"`
+		UpstreamHeader http.Header `json:"uh"`
+		ImageString    string      `json:"im"` // []byte would trigger implicit base64 ops, our output is ascii85
+	}
+	withString.Status = r.Status
+	withString.UpstreamHeader = r.UpstreamHeader
+
+	var buffer bytes.Buffer
+	var err error
+	a85Enc := ascii85.NewEncoder(&buffer)
+	_, err = a85Enc.Write(r.Image)
+	if err != nil {
+		return nil, err
+	}
+	err = a85Enc.Close()
+	if err != nil {
+		return nil, err
+	}
+	withString.ImageString = buffer.String()
+
+	var jsonBuffer bytes.Buffer
+	jsonEnc := json.NewEncoder(&jsonBuffer)
+	jsonEnc.SetEscapeHTML(false) // No need to prepare this to be accidentally parsed as HTNL
+	err = jsonEnc.Encode(withString)
+	if err != nil {
+		return nil, err
+	}
+	return jsonBuffer.Bytes(), nil
+}
+
+func (r *LambdaTransformResponse) UnmarshalJSON(bs []byte) error {
+	var withString struct {
+		Status         int         `json:"st"`
+		UpstreamHeader http.Header `json:"uh"`
+		ImageString    string      `json:"im"` // []byte would trigger implicit base64 ops, and our input could be ascii85
+	}
+	err := json.Unmarshal(bs, &withString)
+	if err != nil {
+		return err
+	}
+
+	// Easy cases
+	r.Status = withString.Status
+	r.UpstreamHeader = withString.UpstreamHeader
+
+	var b64Err error
+	r.Image, b64Err = ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, strings.NewReader(withString.ImageString)))
+	if b64Err != nil {
+		// Try ascii85
+		var a85Err error
+		r.Image, a85Err = ioutil.ReadAll(ascii85.NewDecoder(strings.NewReader(withString.ImageString)))
+		if a85Err != nil {
+			return fmt.Errorf("multifail: %s; %s", b64Err.Error(), a85Err.Error())
+		}
+	}
+	return nil
 }
 
 type LambdaClient interface {
